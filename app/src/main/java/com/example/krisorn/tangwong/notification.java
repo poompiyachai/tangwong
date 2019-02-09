c19ab5fe56643593897a030c63f13ab3944baa0a
poompiyachai <poompiyachai@gmail.com>
2019-02-09 22:42:20 +0700

    ui_update
    ui_update

--- app/src/main/java/com/example/krisorn/tangwong/notification.java
+++ app/src/main/java/com/example/krisorn/tangwong/notification.java
@@ -158,13 +158,10 @@ public class notification extends AppCompatActivity {
                     EditText message = findViewById (R.id.text);
 
                     //  Log.d("aasd",dataSnapshot.child ("room").child (id).child ("q").child (iduser).child ("noti_status").getValue ((String.class)));
-                    if(!(iduser.equals (String.valueOf(0))))
-                    {
+                    if(!(iduser.equals (String.valueOf(0)))) {
 
-                        if(iduser.equals  (String.valueOf(a)))
-                        {
-                            for (long i=1;i<=ALL;i++)
-                            {
+                        if (iduser.equals (String.valueOf (a))) {
+                            for (long i = 1; i <= ALL; i++) {
                                 if (dataSnapshot.child ("room").child (id).child ("people_live").child (String.valueOf (i)).child ("noti_status").getValue ((String.class)).equals ("1")) {
                                     String tempuid = dataSnapshot.child ("room").child (id).child ("people_live").child (String.valueOf (i)).child ("uid").getValue ((String.class));
                                     mDatabase.child ("user").child (tempuid).child ("notification").child ("status").setValue ("1");
@@ -172,9 +169,7 @@ public class notification extends AppCompatActivity {
                                     mDatabase.child ("user").child (tempuid).child ("notification").child ("room").setValue (id);
                                 }
                             }
-                        }
-                        else
-                        {
+                        } else {
                             if (dataSnapshot.child ("room").child (id).child ("people_live").child (iduser).child ("noti_status").getValue ((String.class)).equals ("1")) {
                                 //roomq = findViewById(R.id.roomid).toString ();
                                 String tempuid = dataSnapshot.child ("room").child (id).child ("people_live").child (iduser).child ("uid").getValue ((String.class));
@@ -186,8 +181,8 @@ public class notification extends AppCompatActivity {
                                 mDatabase.child ("user").child (tempuid).child ("notification").child ("room").setValue (id);
                             }
                         }
+                    }
 
-              
                 }
 
 
